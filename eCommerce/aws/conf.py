6472f650a7dee5d6d3c7d0669307467a82903424
Ben Hutton <admin@email.com>
2020-10-08 15:25:27 +0100

    added amazon util folder

---
+++ eCommerce/aws/conf.py
@@ -0,0 +1,35 @@
+import datetime
+import os
+
+# os.environ.get()
+AWS_GROUP_NAME = os.environ.get("AWS_GROUP_NAME", None)
+AWS_USERNAME = os.environ.get("AWS_USERNAME", None)
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID", None)
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY", None)
+
+AWS_FILE_EXPIRE = 200
+AWS_PRELOAD_METADATA = True
+AWS_QUERYSTRING_AUTH = False
+
+DEFAULT_FILE_STORAGE = os.environ.get("DEFAULT_FILE_STORAGE", None)
+STATICFILES_STORAGE = os.environ.get("STATICFILES_STORAGE", None)
+AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME", None)
+S3DIRECT_REGION = os.environ.get("S3DIRECT_REGION", None)
+S3_URL = '//%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+MEDIA_URL = '//%s.s3.amazonaws.com/media/' % AWS_STORAGE_BUCKET_NAME
+MEDIA_ROOT = MEDIA_URL
+STATIC_URL = S3_URL + 'static/'
+ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
+two_months = datetime.timedelta(days=61)
+date_two_months_later = datetime.date.today() + two_months
+expires = date_two_months_later.strftime("%A, %d %B %Y 20:00:00 GMT")
+
+AWS_HEADERS = { 
+'Expires': expires,
+'Cache-Control': 'max-age=%d' % (int(two_months.total_seconds()), ),
+}
+
+PROTECTED_DIR_NAME = 'protected'
+PROTECTED_MEDIA_URL = '//%s.s3.amazonaws.com/%s/' %( AWS_STORAGE_BUCKET_NAME, PROTECTED_DIR_NAME)
+
+AWS_DOWNLOAD_EXPIRE = 5000 #(0ptional, in milliseconds)
