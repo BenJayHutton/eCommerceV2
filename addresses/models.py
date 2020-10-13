800edc3e4e681f149db49d1bab39d7063c1b8a60
Ben Hutton <23144090+BenJayHutton@users.noreply.github>
2020-10-13 23:59:40 +0100

    amended forign key field so on_delete it's set to NULL

--- addresses/models.py
+++ addresses/models.py
@@ -13,7 +13,7 @@ LIST_OF_COUNTRIES = (
 )
 
 class Address(models.Model):
-    billing_profile  = models.ForeignKey(BillingProfile, on_delete=models.CASCADE)
+    billing_profile  = models.ForeignKey(BillingProfile, null=True, on_delete=models.SET_NULL)
     address_type   = models.CharField(max_length=12, choices=ADDRESS_TYPE)
     address_line_1   = models.CharField(max_length=120)
     address_line_2   = models.CharField(max_length=120, null=True, blank=True)
