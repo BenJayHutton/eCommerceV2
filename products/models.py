cace4688ced4d4d676f8d5f33d7be9fda9752d6b
Ben Hutton <23144090+BenJayHutton@users.noreply.github>
2020-10-14 18:16:07 +0100

    added vat to products

--- products/models.py
+++ products/models.py
@@ -77,6 +77,7 @@ class Product(models.Model):
     slug            = models.SlugField(blank=True, unique=True)
     description     = models.TextField()
     price           = models.DecimalField(max_digits=10, decimal_places=2,  default=0.00)
+    vat             = models.DecimalField(max_digits=10, decimal_places=2,  default=0.00)
     image           = models.ImageField(upload_to='products/', null=True, blank=True)
     featured        = models.BooleanField(default=False)
     quantity        = models.IntegerField(default=0)
