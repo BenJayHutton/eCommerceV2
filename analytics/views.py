b0185a3221708fda8949e1a63b33ffc428156c5e
Ben Hutton <23144090+BenJayHutton@users.noreply.github>
2020-11-11 22:48:46 +0100

    moved sales related javascript to its own file

--- analytics/views.py
+++ analytics/views.py
@@ -5,6 +5,7 @@ from django.views.generic import TemplateView, View
 from django.shortcuts import render
 from django.utils import timezone
 
+import random
 import datetime
 
 from orders.models import Order
@@ -13,13 +14,39 @@ class SalesAjaxView(View):
     def get(self, request, *args, **kwargs):
         data = {}
         if request.user.is_staff:
+            qs = Order.objects.all().by_weeks_range(weeks_ago=5, number_of_weeks=5)
             if request.GET.get('type') == 'week':
-                data['labels'] = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-                data['data'] = [123, 256, 190, 209, 100, 300, 280]
+                days = 7
+                start_date = timezone.now().today() - datetime.timedelta(days=days-1)
+                datetime_list=[]
+                labels=[]
+                salesItems=[]
+                for x in range(0,days):
+                    new_time = start_date + datetime.timedelta(days=x)
+                    datetime_list.append(
+                        new_time
+                    )
+                    labels.append(
+                        new_time.strftime("%a")
+                    )
+                    new_qs = qs.filter(updated__day=new_time.day, updated__month=new_time.month)
+                    day_total = new_qs.totals_data()['total__sum'] or 0
+                    salesItems.append(
+                        day_total
+                    )
+                    #print(datetime_list)
+                data['labels'] = labels
+                data['data'] = salesItems
             
             if request.GET.get('type') == '4weeks':
-                data['labels'] = ['Last Week', 'Two Weeks Ago', 'Three Weeks Ago', 'Four Weeks Ago']
-                data['data'] = [123, 256, 190, 209]
+                data['labels'] = ['Four Weeks Ago', 'Three Weeks Ago', 'Couple weeks ago','Last Week', 'This Week']
+                current = 5
+                data['data'] = []
+                for i in range(0,5):
+                    new_qs = qs.by_weeks_range(weeks_ago=current, number_of_weeks=1)
+                    sales_total = new_qs.totals_data()['total__sum'] or 0
+                    data['data'].append(sales_total)
+                    current -= 1
         return JsonResponse(data)
 
 class SalesView(LoginRequiredMixin, TemplateView):
@@ -34,7 +61,9 @@ class SalesView(LoginRequiredMixin, TemplateView):
     def get_context_data(self, *args, **kwargs):
         context = super(SalesView, self).get_context_data(*args, **kwargs)
         qs = Order.objects.all().by_weeks_range(weeks_ago=10, number_of_weeks=10)
-        context['today'] = qs.by_range(start_date=timezone.now().date()).get_sales_breakdown()
+        start_date = timezone.now().date()
+        today_data = qs.by_range(start_date=timezone.now().date()).get_sales_breakdown()
+        context['today'] = today_data
         context['this_week'] = qs.by_weeks_range(weeks_ago=1, number_of_weeks=1).get_sales_breakdown()
         context['last_four_weeks'] = qs.by_weeks_range(weeks_ago=5, number_of_weeks=4).get_sales_breakdown()
         return context
--- static_my_proj/js/ecommerce.sales.js
+++ static_my_proj/js/ecommerce.sales.js
@@ -1,52 +1,52 @@
 $(document).ready(function(){
-  function renderChart(id, data, labels){
-    //var ctx = document.getElementById('myChart').getContext('2d');
-    var ctx = $('#' + id)
-    var myChart = new Chart(ctx, {
-        type: 'line',
-        data: {
-            labels: labels,
-            datasets: [{
-                label: 'Sales',
-                data: data,
-                backgroundColor: 'rgba(75, 199, 132, 0.3)',
-                borderColor: 'rgba(75, 199, 132, 1)'
-            }]
-        },
-        options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true
-                    }
+    function renderChart(id, data, labels){
+        var ctx = $('#' + id);
+        var myChart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: labels,
+                datasets: [{
+                    label: 'Sales',
+                    data: data,
+                    backgroundColor: 'rgba(100, 255, 34, .2)',
+                    borderColor: 'rgba(100, 255, 34, .6)',
                 }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                },
             }
-        }
-    });
-  }
-  
-  function getSalesData(id, type){
-    var url = '/analytics/sales/data/'
-    var method = 'GET'
-    var data = {"type": type}
-    $.ajax({
-      url: url,
-      method:method,
-      data:data,
-      success: function(responseData){
-        renderChart(id, responseData.data, responseData.labels)
-      }, error: function(error){
-        $.alert("an error occured")
-      }
-    })
-  }
-
-  
-  var chartsToRender = $('.sss-render-chart')
-  $.each(chartsToRender, function(index, html){
-    var $this = $(this)
-    if($this.attr('id') && $this.attr('data-type')){
-      getSalesData($this.attr('id'), $this.attr('data-type'))
+        });
     }
-  })
+
+    function getSalesData(id, type){
+        var url = '/analytics/sales/data/'
+        var method = 'GET'
+        var data = {"type":type}
+
+        $.ajax({
+        url: url,
+        method: method,
+        data:data,
+        success: function(responseData){
+            renderChart(id, responseData.data, responseData.labels)
+        },
+        error: function(error){
+            $.alert("An error occurred")
+        }
+        })
+    }
+    
+    var chartsToRender = $('.bjh-render-chart')
+    $.each(chartsToRender, function(index, html){
+        var $this = $(this)
+        if($this.attr('id') && $this.attr('data-type')){
+            getSalesData($this.attr('id'), $this.attr('data-type'))
+        }
+    })
 })
