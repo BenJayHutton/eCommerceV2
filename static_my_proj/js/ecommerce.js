eb534d7d72d433d1415e7ea7d6f8aae130a7054b
Ben Hutton <23144090+BenJayHutton@users.noreply.github>
2020-11-13 19:07:58 +0100

    cleaned up code for adding/removing/amending products to the cart

--- static_my_proj/js/ecommerce.js
+++ static_my_proj/js/ecommerce.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    var productForm = $(".form-product-ajax")
+    var formData = $(".form-product-ajax")
 
     function getOwnedProduct(productId, submitSpan){
         var actionEndPoint = '/orders/endpoint/verify/ownership/';
@@ -29,7 +29,7 @@ $(document).ready(function(){
     }
 
     
-    $.each(productForm, function(index, object){
+    $.each(formData, function(index, object){
         var $this = $(this);
         var isUser = $this.attr("data-user");
         var submitSpan = $this.find(".submit-span");
@@ -44,89 +44,55 @@ $(document).ready(function(){
         }
         
     })
-
-    productForm.submit(function(event){
+    
+    formData.on('submit', function(event) {
         event.preventDefault();
         var thisForm = $(this)
-        var actionEndPoint = thisForm.attr("action");
-        var httpMethod = thisForm.attr("method");
-        var submitBtnAdd = thisForm.find(".cart_item_add");
-        var submitBtnRemove = thisForm.find(".cart_item_remove");
-        var submitBtnUpdate = thisForm.find(".cart_item_update");
-        var cartBtnRemove = thisForm.find(".cart_item_remove");
-        var formData = thisForm.serialize();
+        var cartSubmitBtn = event.originalEvent.submitter.attributes[3].value;
+        var url = '/cart/update/'
+        var method = 'POST'
+        var data = thisForm.serialize();
 
-        console.log("cartBtnRemove", cartBtnRemove)
+        console.log("cartSubmitBtn", cartSubmitBtn)
 
-        if (typeof cartBtnRemove !== 'undefined' && typeof cartBtnRemove.prevObject[0][5] !== 'undefined'){
-            cartBtnRemove.click(function(event){
-                var btnSerialzeUpdate = cartBtnRemove.prevObject[0][5].name +"="+ cartBtnRemove.prevObject[0][5].value;
-                formData = formData+"&"+btnSerialzeUpdate;
-            })
-            
+        if(cartSubmitBtn == "cart_item_update"){
+            data = data+"&"+cartSubmitBtn+"=true"
+        }else if(cartSubmitBtn == "cart_item_remove"){
+            data = data+"&"+cartSubmitBtn+"=true"
+        }else if(cartSubmitBtn == "cart_item_add"){
+            data = data+"&"+cartSubmitBtn+"=true"
         }
+        upDateOrder(url, method, data, thisForm)
+    });
 
-        if (typeof submitBtnUpdate !== 'undefined' && typeof submitBtnUpdate.prevObject[0][4] !== 'undefined'){
-            var btnSerialzeUpdate = submitBtnUpdate.prevObject[0][4].name +"="+ submitBtnUpdate.prevObject[0][4].value;
-            formData = formData+"&"+btnSerialzeUpdate;
-        }
-
-        if (typeof submitBtnUpdate !== 'undefined' && typeof submitBtnUpdate.prevObject[0][4] !== 'undefined'){
-            var btnSerialzeUpdate = submitBtnUpdate.prevObject[0][4].name +"="+ submitBtnUpdate.prevObject[0][4].value;
-            formData = formData+"&"+btnSerialzeUpdate;
-        }
-        
-        if (typeof submitBtnAdd !== 'undefined' && typeof submitBtnAdd.prevObject[0][3] !== 'undefined'){
-            var btnSerialzeAdd = submitBtnAdd.prevObject[0][3].name +"="+ submitBtnAdd.prevObject[0][3].value;
-            formData = formData+"&"+btnSerialzeAdd;
-        }else{
-            var btnSerialzeAdd = submitBtnAdd.prevObject[0][2].name +"="+ submitBtnAdd.prevObject[0][2].value;
-            formData = formData+"&"+btnSerialzeAdd;
-        }
-        
-        if (typeof submitBtnRemove !== 'undefined' && typeof submitBtnRemove.prevObject[0][3] !== 'undefined'){
-            var btnSerialzeRemove = submitBtnRemove.prevObject[0][3].name +"="+ submitBtnRemove.prevObject[0][3].value;
-            formData = formData+"&"+btnSerialzeRemove;
-        }else{
-            var btnSerialzeRemove = submitBtnRemove.prevObject[0][2].name +"="+ submitBtnRemove.prevObject[0][2].value;
-            formData = formData+"&"+btnSerialzeRemove;
-        }
-
+    function upDateOrder(url, method,formData, thisForm){
         $.ajax({
-        url: actionEndPoint,
-        method: httpMethod,
-        data: formData,
-        success: function(data){
-            var submitSpan = thisForm.find(".submit-span")
-            var cartUpdateSpanTotal = $(".cart-total")
-            var cartUpdateSpanVatTotal = $(".cart-vattotal")
-            var cartUpdateSpanSubtotal = $(".cart-subtotal")
-            if(data.updated){
-                cartUpdateSpanTotal.html(data.cart_total)
-                cartUpdateSpanVatTotal.html(data.cart_vat)
-                cartUpdateSpanSubtotal.html(data.cart_subtotal)
-            }
-            if(data.added){
-                submitSpan.html('<input type="hidden" id="cart_item_id" name="cart_item_id" value="'+data.cart_item_id+'"><div class="btn-group"><a class="btn btn-link" href="/cart/">In cart</a> <button type="submit" id="cart_item_remove" name="cart_item_remove" value="true" class="btn btn-danger">Remove</button></div>')
-                console.log(data.cart_item_id)
-            }else{
-                submitSpan.html('<input id="product_quantity" name="product_quantity" type="number" min="1" max="'+data.productQty+'" value="1" /><button type="submit" id="cart_item_add" name="cart_item_add" value="true" class="btn btn-success btn-add">Add to cart</button>')
+            url: url,
+            method: method,
+            data:formData,
+            success: function(data){
+                var submitSpan = thisForm.find(".submit-span")
+                if(data.updated){
+                    var cartUpdateSpanTotal = $(".cart-total")
+                    var cartUpdateSpanVatTotal = $(".cart-vattotal")
+                    var cartUpdateSpanSubtotal = $(".cart-subtotal")
+                    cartUpdateSpanTotal.html(data.cart_total)
+                    cartUpdateSpanVatTotal.html(data.cart_vat)
+                    cartUpdateSpanSubtotal.html(data.cart_subtotal)
+                }
+                if(data.added){
+                    submitSpan.html('<input type="hidden" id="cart_item_id" name="cart_item_id" value="'+data.cart_item_id+'"><div class="btn-group"><a class="btn btn-link" href="/cart/">In cart</a> <button onclick="this.form.submitted=this.value;" type="submit" id="cart_item_remove" name="cart_item_remove" value="true" class="btn btn-danger">Remove</button></div>')
+                    console.log(data.cart_item_id)
+                }
+                if(data.removed){
+                    submitSpan.html('<input id="product_quantity" name="product_quantity" type="number" min="1" max="'+data.productQty+'" value="1" /><button onclick="this.form.submitted=this.value;" type="submit" id="cart_item_add" name="cart_item_add" value="true" class="btn btn-success btn-add">Add to cart</button>')
+                    console.log("data", data)
+                }
                 console.log("data", data)
+            },
+            error: function(error){
+                console.log("error", error)
             }
-            var navbarCount = $(".navbar-cart-count")
-            navbarCount.text(data.cartItemCount)
-            // var currentPath = window.location.href
-            // if (currentPath.indexOf("cart") != -1){
-            // refreshCart()
-            // }
-        },
-        error: function(errorData){
-            $.alert({
-            title: "oops!",
-            content: "an error occurred",
-            theme: "modern",
-            })
-        }
         })
-    })
+    }
 })
