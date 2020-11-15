7def119c345a906c596b1e14370e066150db663d
Ben Hutton <23144090+BenJayHutton@users.noreply.github>
2020-11-15 17:27:30 +0100

    added search option for billing models in admin

--- billing/admin.py
+++ billing/admin.py
@@ -2,6 +2,21 @@ from django.contrib import admin
 
 from .models import BillingProfile, Card, Charge
 
-admin.site.register(BillingProfile)
-admin.site.register(Card)
-admin.site.register(Charge)
+class BillingAdmin(admin.ModelAdmin):
+    search_fields = ['user__email', 'customer_id']
+    class Meta:
+        model = BillingProfile
+
+class CardAdmin(admin.ModelAdmin):
+    search_fields = ['billing_profile__user__email', 'stripe_id']
+    class Meta:
+        model = Card
+
+class ChargeAdmin(admin.ModelAdmin):
+    search_fields = ['billing_profile__user__email', 'stripe_id']
+    class Meta:
+        model = Charge
+
+admin.site.register(BillingProfile, BillingAdmin)
+admin.site.register(Card, CardAdmin)
+admin.site.register(Charge, ChargeAdmin)
