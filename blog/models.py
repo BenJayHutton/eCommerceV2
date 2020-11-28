a186f84bbbd7f302ff4bce8090ad038b9552273e
Ben Hutton <23144090+BenJayHutton@users.noreply.github>
2020-11-28 01:02:08 +0100

    added blog app to admin

--- blog/models.py
+++ blog/models.py
@@ -3,9 +3,12 @@ from django.contrib.auth import get_user_model, authenticate
 
 User = get_user_model()
 
-class blog(models.Model):
+class Blog(models.Model):
     user        = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     subject     = models.CharField(max_length=120)
     text_boy    = models.TextField()
     updated     = models.DateTimeField(auto_now=True)
     timestamp   = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.email + " :- " + self.subject
--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,3 @@
 from django.conf.urls import url, path
 
-
 urlpatterns = []
