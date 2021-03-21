3412117f413ab5271fd310d4b5c6c39a2def35ed
Ben Hutton <23144090+BenJayHutton@users.noreply.github>
2021-03-21 10:07:20 +0000

    refactored code for ebay api

--- ebay/views.py
+++ ebay/views.py
@@ -30,28 +30,26 @@ class EbayShoppingApi(TemplateView):
                "responseencoding={responseencoding}&" \
                "appid={api_key}&" \
                "siteid=0&" \
-               "version=967&" \
-               "QueryKeywords={search}&" \
-               "AvailableItemsOnly=true&" \
-               "MaxEntries=2"
+               "version=967"
 
-    def find_products(self, *args, **kwargs):
+    def find_products(self, **kwargs):
         api_key = kwargs.get("api_key", None)
         search = urllib.parse.quote(kwargs.get("search", None))
         call_name = "FindProducts"
-        url = self.base_url.format(
+        url = self.base_url+"&QueryKeywords={search}&" \
+               "AvailableItemsOnly=true&" \
+               "MaxEntries=2"
+        response = requests.get(url.format(
             base_url=self.base_url,
             callname=call_name,
             api_key=api_key,
             responseencoding=self.responseencoding,
-            search=search)
-
-        response = requests.get(url)
+            search=search))
         return response.json()
 
     def get_category_info(self, **kwargs):
         api_key = kwargs.get("api_key", None)
-        url_search = urllib.parse.quote(kwargs.get("search", " "))
+        item_id = urllib.parse.quote(kwargs.get("itemId", " "))
         call_name = "GetCategoryInfo"
         return self.base_url.format(
                     base_url=self.base_url,
@@ -60,7 +58,7 @@ class EbayShoppingApi(TemplateView):
                     responseencoding=self.responseencoding,
                     search=url_search)
 
-    def get_ebay_time(self):
+    def get_ebay_time(self, **kwargs):
         api_key = kwargs.get("api_key", None)
         url_search = urllib.parse.quote(kwargs.get("search", " "))
         call_name = "GeteBayTime"
@@ -71,29 +69,31 @@ class EbayShoppingApi(TemplateView):
             responseencoding=self.responseencoding,
             search=url_search)
 
-    def get_item_status(self):
+    def get_item_status(self, **kwargs):
         api_key = kwargs.get("api_key", None)
-        url_search = urllib.parse.quote(kwargs.get("search", " "))
+        item_id = urllib.parse.quote(kwargs.get("ItemId", " "))
+        url = self.base_url + "&ItemId={item_id}"
         call_name = "GetItemStatus"
-        return self.base_url.format(
+        return url.format(
             base_url=self.base_url,
             callname=call_name,
             api_key=api_key,
             responseencoding=self.responseencoding,
-            search=url_search)
+            item_id=item_id)
 
-    def get_multiple_items(self):
+    def get_multiple_items(self, **kwargs):
         api_key = kwargs.get("api_key", None)
-        url_search = urllib.parse.quote(kwargs.get("search", " "))
+        url = self.base_url + "&ItemId={item_id}"
+        item_id = urllib.parse.quote(kwargs.get("ItemId", " "))
         call_name = "GetMultipleItems"
-        return self.base_url.format(
+        return url.format(
             base_url=self.base_url,
             callname=call_name,
             api_key=api_key,
             responseencoding=self.responseencoding,
-            search=url_search)
+            item_id=item_id)
 
-    def get_shipping_costs(self):
+    def get_shipping_costs(self, **kwargs):
         api_key = kwargs.get("api_key", None)
         url_search = urllib.parse.quote(kwargs.get("search", " "))
         call_name = "GetShippingCosts"
@@ -104,7 +104,7 @@ class EbayShoppingApi(TemplateView):
             responseencoding=self.responseencoding,
             search=url_search)
 
-    def get_single_item(self):
+    def get_single_item(self, **kwargs):
         api_key = kwargs.get("api_key", None)
         url_search = urllib.parse.quote(kwargs.get("search", " "))
         call_name = "GetSingleItem"
@@ -115,7 +115,7 @@ class EbayShoppingApi(TemplateView):
             responseencoding=self.responseencoding,
             search=url_search)
 
-    def get_user_profile(self):
+    def get_user_profile(self, **kwargs):
         api_key = kwargs.get("api_key", None)
         url_search = urllib.parse.quote(kwargs.get("search", " "))
         call_name = "GetUserProfile"
@@ -133,20 +133,45 @@ class EbaySearchListing(TemplateView):
     EbayMerchendiseApi = EbayMerchendiseApi()
 
     def get(self, request):
+        user = request.user
+        ebay_production_key = EbayAccount.objects.all().filter(user=user).first().production_api_key
+        get_multiple_items = self.EbayShoppingApi.get_multiple_items(api_key=ebay_production_key)
 
         context = {
             'form': self.form_class,
             'EbayMerchendiseApi': self.EbayMerchendiseApi,
-            'EbayShoppingApi': self.EbayShoppingApi,
+            'get_multiple_items': get_multiple_items,
         }
         return render(request, "ebay/index.html", context)
 
     def post(self, request, *args, **kwargs):
+        response = HttpResponse()
         form = self.form_class(request.POST)
         user = request.user
         ebay_production_key = EbayAccount.objects.all().filter(user=user).first().production_api_key
+
         if form.is_valid():
-            result = self.EbayShoppingApi.find_products(search=form.cleaned_data['search'], api_key=ebay_production_key)
+            print(response)
+            form_select_api = form.cleaned_data['api_choice']
+            form_search_api = form.cleaned_data['search']
+            if form_select_api == "FindProducts":
+                result = self.EbayShoppingApi.find_products(search=form_search_api, api_key=ebay_production_key)
+            elif form_select_api == "GetCategoryInfo":
+                result = self.EbayShoppingApi.get_category_info(search=form_search_api, api_key=ebay_production_key)
+            elif form_select_api == "GeteBayTime":
+                result = self.EbayShoppingApi.get_ebay_time(search=form_search_api, api_key=ebay_production_key)
+            elif form_select_api == "GetItemStatus":
+                result = self.EbayShoppingApi.get_item_status(search=form_search_api, api_key=ebay_production_key)
+            elif form_select_api == "GetMultipleItems":
+                result = self.EbayShoppingApi.get_multiple_items(search=form_search_api, api_key=ebay_production_key)
+            elif form_select_api == "GetShippingCosts":
+                result = self.EbayShoppingApi.get_shipping_costs(search=form_search_api, api_key=ebay_production_key)
+            elif form_select_api == "GetSingleItem":
+                result = self.EbayShoppingApi.get_single_item(search=form_search_api, api_key=ebay_production_key)
+            elif form_select_api == "GetUserProfile":
+                result = self.EbayShoppingApi.get_user_profile(search=form_search_api, api_key=ebay_production_key)
+            else:
+                result = None
 
         context = {
             'result': result,
