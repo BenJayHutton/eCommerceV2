aff1174b06c4264dca717bbd96b2a183de27b916
Ben Hutton <23144090+BenJayHutton@users.noreply.github>
2021-04-02 21:43:31 +0100

    amended selection field for blog:create

--- blog/forms.py
+++ blog/forms.py
@@ -5,12 +5,15 @@ from .models import Blog
 
 
 class BlogForm(forms.ModelForm):
+    #years = [(x) for x in range(1900, date.today().year + 1)]
     class Meta:
         now = datetime.datetime.now().date()
+        year_range = tuple([i for i in range(now.year - 2, now.year + 3)])
         model = Blog
         fields = ['title', 'date', 'blog_post', 'tags', 'is_public']
         widgets = {
             "date": forms.SelectDateWidget(
+                years=year_range,
                 attrs={
                     "value": now
                 }
