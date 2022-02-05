fcbcfef3bf05a84769ae924475d81f02e91f2319
Ben H <Ben@benjayhutton.com>
2022-02-05 21:49:26 +0000

    changed booleanfield in marketing.models app. its now inline with django 4

--- marketing/models.py
+++ marketing/models.py
@@ -8,7 +8,7 @@ from .utils import Mailchimp
 class MarketingPreference(models.Model):
     user                    = models.OneToOneField(settings.AUTH_USER_MODEL, null=True, on_delete=models.SET_NULL)
     subscribed              = models.BooleanField(default=True)
-    mailchimp_subscribed    = models.NullBooleanField(blank=True)
+    mailchimp_subscribed    = models.BooleanField(default=True)
     mailchimp_msg           = models.TextField(null=True, blank=True)
     timestamp               = models.DateTimeField(auto_now_add=True)
     updated                 = models.DateTimeField(auto_now=True)
