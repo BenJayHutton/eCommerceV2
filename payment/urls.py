443b987728537ebecb018ba3018662fbf0680b00
Ben H <Ben@benjayhutton.com>
2022-04-18 16:52:58 +0100

    corrected typo in payments.urls

--- payment/urls.py
+++ payment/urls.py
@@ -5,5 +5,5 @@ from .views import PaymentHome, Paypal, Stripe
 urlpatterns = [
     re_path(r'^$', PaymentHome.as_view(), name='home'),
     re_path(r'^paypal/$', Paypal.as_view(), name='paypal'),
-    re_path(r'^stripe/$', Stripe.as_view(), name='paypal'),
+    re_path(r'^stripe/$', Stripe.as_view(), name='stripe'),
 ]
